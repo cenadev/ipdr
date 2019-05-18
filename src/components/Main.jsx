60097571a347c0e3724e1a9db0cdece0dec5ba68
cleisonsantos <css.cleison@outlook.com>
2019-05-18 11:35:57 -0300

    Criado página de welcome e programação do titulo.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import { React, userState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import './Main.css';
 import Header from './Header';
 import Cadastrar from '../pages/Cadastrar'
 import Membros from '../pages/Membros'
+import Welcome from './Welcome';
+
 export default function Main() {
+  const [ titlePage, setTitlePage ] = userState();
+
   return (
     <>
       <Header />
       <div className="Main">
         <BrowserRouter>
         <Switch>
+          <Route path="/" exact component={Welcome}/>
           <Route path="/cadastrar" component={Cadastrar}/>
           <Route path="/membros" component={Membros}/>
         </Switch>
