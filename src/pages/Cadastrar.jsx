32ed127aa6d033e6c9841f7f9b67a43cf1763c20
cleisonsantos <css.cleison@outlook.com>
2019-05-20 07:08:37 -0300

    Formulario sendo construido.

--- src/pages/Cadastrar.jsx
+++ src/pages/Cadastrar.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 export default function Cadastrar() {
     return(
         <div className="Cadastrar">
-            <h2>Cadastrar</h2>
+            <h2>Ficha de Membro</h2>
+            <form action="" className="form-member">
+                <input type="image" src="" alt=""/>
+                <input type="text" name="name"/>
+                <input type="radio" name="gender" id="f"/>
+                <input type="radio" name="gender" id="m"/>
+                <input type="text" name="pob"/>
+                <input type="date" name="dob"/>
+                <input type="text" name="nationality"/>
+                <input type="text" name=""/>
+            </form>
         </div>
     );
 }
