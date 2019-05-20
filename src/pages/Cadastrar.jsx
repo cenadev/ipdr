eb2f05815152706bfe05453d79ffd2059c43d9b3
cleisonsantos <css.cleison@outlook.com>
2019-05-20 14:37:14 -0300

    Adicionado os components do materia ui.

--- src/pages/Cadastrar.jsx
+++ src/pages/Cadastrar.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 export default function Cadastrar() {
-    return(
+    return (
         <div className="Cadastrar">
             <h2>Ficha de Membro</h2>
             <form action="" className="form-member">
-                <input type="image" src="" alt=""/>
-                <input type="text" name="name"/>
-                <input type="radio" name="gender" id="f"/>
-                <input type="radio" name="gender" id="m"/>
-                <input type="text" name="pob"/>
-                <input type="date" name="dob"/>
-                <input type="text" name="nationality"/>
-                <input type="text" name=""/>
+                <FormControl>
+                    <TextField label="Nome Completo" margin="normal" variant="outlined" />
+                </FormControl>
+                <FormControl component="fieldset" >
+                    <FormLabel component="legend">Gênero</FormLabel>
+                    <RadioGroup
+                        aria-label="Gender"
+                        name="gender1"
+                    >
+                        <FormControlLabel value="female" control={<Radio />} label="Female" />
+                        <FormControlLabel value="male" control={<Radio />} label="Male" />
+                    </RadioGroup>
+                </FormControl>
+
+                <TextField label="Natalidade" margin="normal" variant="outlined" />
+                <TextField
+                    id="date"
+                    label="Data de Nascimento"
+                    type="date"
+                    defaultValue="2017-05-24"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    margin="normal"
+                    variant="outlined"
+                />
+                <TextField label="Nacionalidade" margin="normal" variant="outlined" />
+                <TextField label="Nome Completo" margin="normal" variant="outlined" />
+
             </form>
         </div>
     );
